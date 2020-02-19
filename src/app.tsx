e692e14a1eb648128158e6459e6556c174f9f88e
oderindeolayemi <oderinde.yemi@gmail.com>
2020-02-19 11:38:47 +0100

    set up typescript, sass using parcel

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './app.scss';
+
+const App = ()=>{
+    return <h1>Hi</h1>;
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('app')
+);
